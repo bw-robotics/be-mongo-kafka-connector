c85228dce187d5e3ab13ec3c89bbf9972420f206
Ognjen <ognjen.a@bluewhite.co>
2024-04-04 13:07:46 +0200

    Add missing fields for getting topic name

--- src/main/java/com/mongodb/kafka/connect/source/topic/mapping/CustomDataTypeTopicMapper.java
+++ src/main/java/com/mongodb/kafka/connect/source/topic/mapping/CustomDataTypeTopicMapper.java
@@ -16,12 +16,15 @@ public class CustomDataTypeTopicMapper implements TopicMapper {
   public static final String OPERATION_TYPE_FIELD = "operationType";
   public static final String FULL_DOCUMENT_FIELD = "fullDocument";
   public static final String DATA_TYPE_FIELD = "dataType";
+  public static final String NS_FIELD = "ns";
+  public static final String COLLECTION_FIELD = "coll";
+  public static final String UPDATED_DESCRIPTION_FIELD = "updateDescription";
+  public static final String INSERT_OPERATION_TYPE = "insert";
 
   private String separator;
   private String prefix;
   private String suffix;
   private String database;
-  private String collection;
   private String dlqTopicName;
 
   @Override
@@ -33,30 +36,40 @@ public class CustomDataTypeTopicMapper implements TopicMapper {
     this.prefix = prefix.isEmpty() ? prefix : prefix + separator;
     this.suffix = suffix.isEmpty() ? suffix : separator + suffix;
     this.database = config.getString(DATABASE_CONFIG);
-    this.collection = config.getString(COLLECTION_CONFIG);
     this.dlqTopicName = config.getString(OVERRIDE_ERRORS_DEAD_LETTER_QUEUE_TOPIC_NAME_CONFIG);
   }
 
   @Override
   public String getTopic(final BsonDocument changeStreamDocument) {
-  final boolean isValidJsonFields = isValidJsonFields(changeStreamDocument);
+    final boolean isValidJsonFields = isValidJsonFields(changeStreamDocument);
 
-  if (!isValidJsonFields) {
-    LOGGER.error("Json fields are not valid in document: {}", changeStreamDocument);
-    return dlqTopicName;
-  }
+    if (!isValidJsonFields) {
+      LOGGER.error("Json fields are not valid in document: {}", changeStreamDocument);
+      return dlqTopicName;
+    }
 
     try {
-      final BsonDocument fullDocument = changeStreamDocument.get(FULL_DOCUMENT_FIELD).asDocument();
+      final BsonDocument nsDocument = changeStreamDocument.get(NS_FIELD).asDocument();
+      final String collection = nsDocument.get(COLLECTION_FIELD).asString().getValue();
+      final String operationType =
+          changeStreamDocument.get(OPERATION_TYPE_FIELD).asString().getValue();
 
+      if (!INSERT_OPERATION_TYPE.equals(operationType)) {
+        return getTopicName(collection);
+      }
+
+      final BsonDocument fullDocument = changeStreamDocument.get(FULL_DOCUMENT_FIELD).asDocument();
       if (!fullDocument.containsKey(DATA_TYPE_FIELD)) {
-        return getTopicName();
+        return getTopicName(collection);
       }
 
       final String dataType = fullDocument.get(DATA_TYPE_FIELD).asString().getValue();
-      return getTopicName(dataType);
+      return getTopicName(dataType, collection);
     } catch (Exception e) {
-      LOGGER.error("Exception occurred while trying to get topic name", e);
+      LOGGER.error(
+          "Exception occurred while trying to get topic name for document: {}",
+          changeStreamDocument,
+          e);
       return dlqTopicName;
     }
   }
@@ -66,18 +79,23 @@ public class CustomDataTypeTopicMapper implements TopicMapper {
       return false;
     }
 
-    if (!changeStreamDocument.containsKey(FULL_DOCUMENT_FIELD)) {
+    if (!changeStreamDocument.containsKey(FULL_DOCUMENT_FIELD)
+        && !changeStreamDocument.containsKey(UPDATED_DESCRIPTION_FIELD)) {
+      return false;
+    }
+
+    if (!changeStreamDocument.containsKey(NS_FIELD)) {
       return false;
     }
 
     return true;
   }
 
-  private String getTopicName() {
-      return String.join("", prefix, database, separator, collection, suffix);
+  private String getTopicName(final String collection) {
+    return String.join("", prefix, database, separator, collection, suffix);
   }
 
-  private String getTopicName(final String dataType) {
-      return String.join("", prefix, database, separator, collection, separator, dataType, suffix);
+  private String getTopicName(final String dataType, final String collection) {
+    return String.join("", prefix, database, separator, collection, separator, dataType, suffix);
   }
 }
